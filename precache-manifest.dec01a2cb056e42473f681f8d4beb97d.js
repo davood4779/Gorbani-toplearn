3cc44dbbb4e128b8922aec5da89a165a283a7825
davood4779 <61736325+davood4779@users.noreply.github.com>
2022-04-18 05:28:49 -0700

    Add files via upload

---
+++ precache-manifest.dec01a2cb056e42473f681f8d4beb97d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c10bb9049e5cb3276d2ac80acfab3e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29c791c60f669db62d89",
+    "url": "/static/js/2.39b9cfa5.chunk.js"
+  },
+  {
+    "revision": "0ce703aaf3831345a8e52d18a2881625",
+    "url": "/static/js/2.39b9cfa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50e4e9a1f506c8d09164",
+    "url": "/static/js/main.a1569966.chunk.js"
+  },
+  {
+    "revision": "5d870d4ea17c9ecbb24f",
+    "url": "/static/js/runtime-main.4ae6e250.js"
+  }
+]);
